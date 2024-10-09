eee44517327c8aeb140f04d63c9e4c0f6eff5bad
Orvidius <orvidius@orvidius.com>
2024-10-09 00:00:01 -0400

    auto commit 2024-10-09 00:00:01

---
+++ scripts/scplist.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+scp lagrange-capable-stars.csv www@services:/www/edastro.com/mapcharts/files/  
+scp -P222 Ammonia-worlds.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 Earth-like-worlds.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 Helium-gas-giants.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 Helium-rich-giants.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 Life-giants.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 Water-giants.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 distant-planets.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 eccentric-orbits-landable.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 eccentric-orbits.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 gas-giants-as-moons.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 metal-rich-terraformables.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 shortest-rotation-planets.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 zero-distance-planets.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 A-class-supergiants.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 B-class-supergiants.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 Black-Holes.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 Carbon-C-stars.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 Carbon-stars.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 K-class-giants.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 O-class-stars.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 Red-SuperGiants.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 WhiteDwarf-Rare-Subtypes.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 Wolf-Rayet-stars.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 distant-stars.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 herbig-stars.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 moons-of-ELWs.csv www@services:/www/edastro.com/mapcharts/files/
+scp nearest-sol-discoveries.csv www@services:/www/edastro.com/mapcharts/files/ 
+scp -P222 sector-discovery.csv www@services:/www/edastro.com/mapcharts/files/ 
+scp -P222 database-stats.csv www@services:/www/edastro.com/mapcharts/files/ 
+scp inclined-moons-near-rings.csv www@services:/www/edastro.com/mapcharts/files/ 
+scp -P222 ELW-multiples.csv www@services:/www/edastro.com/mapcharts/files/ 
+scp close-landables.csv www@services:/www/edastro.com/mapcharts/files/
+scp -P222 Nested-Moons.csv www@services:/www/edastro.com/mapcharts/files/ 
+scp -P222 edge-systems.csv www@services:/www/edastro.com/mapcharts/files/ 
+scp -P222 hot-gasgiants.csv www@services:/www/edastro.com/mapcharts/files/ 
+scp -P222 hot-jupiters.csv www@services:/www/edastro.com/mapcharts/files/ 
+scp -P222 unknown-stars.csv www@services:/www/edastro.com/mapcharts/files/ 
+scp -P222 body-counts.csv www@services:/www/edastro.com/mapcharts/files/ 
+scp -P222 binary-ELW.csv www@services:/www/edastro.com/mapcharts/files/ 
+scp -P222 neutron-stars.csv www@services:/www/edastro.com/mapcharts/files/ 
+scp discovery-dates.csv discovery-months.csv www@services:/www/edastro.com/mapcharts/files/
+scp boxel-stats.csv www@services:/www/edastro.com/mapcharts/files/
+scp popular-carrier-names.csv www@services:/www/edastro.com/mapcharts/files/
+scp date-erosion.csv www@services:/www/edastro.com/mapcharts/files/
+scp multiple-star-planets.csv www@services:/www/edastro.com/mapcharts/files/
+scp water-worlds-g-class.csv www@services:/www/edastro.com/mapcharts/files/
+scp short-period-planets.csv www@services:/www/edastro.com/mapcharts/files/
+scp ringed-stars.csv www@services:/www/edastro.com/mapcharts/files/
+scp valuable-planet-systems.csv www@services:/www/edastro.com/mapcharts/files/
+scp tightest-binary-stars.csv www@services:/www/edastro.com/mapcharts/files/
+scp tightest-binary-stars-surface.csv www@services:/www/edastro.com/mapcharts/files/
+scp ELW-moons.csv www@services:/www/edastro.com/mapcharts/files/
+scp terraformable-systems.csv www@services:/www/edastro.com/mapcharts/files/
+scp systems-without-coordinates.csv www@services:/www/edastro.com/mapcharts/files/
+scp stations.csv www@services:/www/edastro.com/mapcharts/files/
+scp tidally-locked-unequal.csv www@services:/www/edastro.com/mapcharts/files/
+scp landables-closest-approach.csv www@services:/www/edastro.com/mapcharts/files/
+scp sol-like-systems.csv www@services:/www/edastro.com/mapcharts/files/
+scp codex-data.csv www@services:/www/edastro.com/mapcharts/files/
+scp fleetcarriers.csv www@services:/www/edastro.com/mapcharts/files/
