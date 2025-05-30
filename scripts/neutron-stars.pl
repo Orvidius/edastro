ac02596341551c78fd6807d9d225659f4849ae85
Orvidius <orvidius@orvidius.com>
2025-05-30 00:00:02 -0400

    auto commit 2025-05-30 00:00:02

--- scripts/neutron-stars.pl
+++ scripts/neutron-stars.pl
@@ -15,12 +15,12 @@ use ATOMS qw(make_csv epoch2date date2epoch);
 
 show_queries(0);
 
-print make_csv('ID64 SystemAddress','EDSM System ID','Name')."\r\n";
+print make_csv('ID64 SystemAddress','EDSM System ID','Name','Rotation Period Seconds','Region ID','X','Y','Z')."\r\n";
 
-my @rows = db_mysql('elite',"select systemId64,systemId,name from stars where subType='Neutron star' and deletionState=0 order by name");
+my @rows = db_mysql('elite',"select systemId64,systemId,stars.name,rotationalPeriod,region,coord_x,coord_y,coord_z from stars,systems where subType='Neutron star' and systemId64=id64 and stars.deletionState=0 and systems.deletionState=0 order by name");
 while (@rows) {
 	my $r = shift @rows;
-	print make_csv($$r{systemId64},$$r{systemId},$$r{name})."\r\n";
+	print make_csv($$r{systemId64},$$r{systemId},$$r{name},int($$r{rotationalPeriod}*86400000)/1000,$$r{region},$$r{coord_x},$$r{coord_y},$$r{coord_z})."\r\n";
 }
 
 exit;
